8b4b99614a35e28cca3086d434e5d50c3042f9d3
VS_Online <maxhank22@gmail.com>
2017-01-18 23:38:47 +0800

    加入專案檔案。

---
+++ PotterShoppingCart.Tests/ShoppingCartTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PotterShoppingCart;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PotterShoppingCart.Tests
+{
+    [TestClass()]
+    public class ShoppingCartTests
+    {
+        [TestMethod()]
+        public void 第一集買了一本其他都沒買價格應為100元()
+        {
+            IEnumerable<Product> products = new List<Product>
+            {
+                new Product { name="哈利波特第一集",kind="哈利波特",epiSode="1",setPrice=100,qty=1 }
+            };
+
+            //Arrange
+            var expected = 100;
+            var shoppingcart = new ShoppingCart();
+            //Act
+            var actual = shoppingcart.SumTotal(products);
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ PotterShoppingCart/ShoppingCart.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PotterShoppingCart
+{
+    public class ShoppingCart
+    {
+        public int SumTotal(IEnumerable<Product> products)
+        {
+            int result = 0;
+            return result;
+        }
+    }
+
+    public class Product
+    {
+        /// <summary>
+        /// 書名
+        /// </summary>
+        public string name { get; set; }
+        /// <summary>
+        /// 系列別
+        /// </summary>
+        public string kind { get; set; }
+        /// <summary>
+        /// 集數
+        /// </summary>
+        public string epiSode { get; set; }
+        /// <summary>
+        /// 價格
+        /// </summary>
+        public int setPrice { get; set; }
+        
+        public int qty { get; set; }
+    }
+}
