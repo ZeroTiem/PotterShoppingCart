05f0330a4874a7a7fc5258c29cf8a8d828146491
VS_Online <maxhank22@gmail.com>
2017-01-19 00:23:10 +0800

    [紅燈]一次買了整套一二三四五集各買了一本每本100元得到折扣75折總金額為375元

--- PotterShoppingCart.Tests/ShoppingCartTests.cs
+++ PotterShoppingCart.Tests/ShoppingCartTests.cs
@@ -84,6 +84,29 @@ namespace PotterShoppingCart.Tests
             Assert.AreEqual(expected, actual);
         }
 
+        [TestMethod()]
+        public void 一次買了整套一二三四五集各買了一本每本100元得到折扣75折總金額為375元()
+        {
+            IEnumerable<Product> products = new List<Product>
+            {
+                new Product { name="哈利波特第一集",kind="哈利波特",epiSode="1",setPrice=100,qty=1 },
+                new Product { name="哈利波特第二集",kind="哈利波特",epiSode="2",setPrice=100,qty=1 },
+                new Product { name="哈利波特第三集",kind="哈利波特",epiSode="3",setPrice=100,qty=1 },
+                new Product { name="哈利波特第四集",kind="哈利波特",epiSode="4",setPrice=100,qty=1 },
+                new Product { name="哈利波特第五集",kind="哈利波特",epiSode="5",setPrice=100,qty=1 }
+            };
+
+            //Arrange
+            var expected = 375;
+            var shoppingcart = new ShoppingCart();
+            //Act
+            var actual = shoppingcart.SumTotal(products);
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        
+
         
     }
 }
